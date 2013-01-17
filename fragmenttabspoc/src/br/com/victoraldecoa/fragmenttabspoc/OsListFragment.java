5f64588e01347b338f8f4d0c121aa0a667073fb4
Victor Maia Aldecoa <victor.maia.a@gmail.com>
2013-01-17 21:34:30 +0100

    Silly modifications

--- fragmenttabspoc/src/br/com/victoraldecoa/fragmenttabspoc/OsListFragment.java
+++ fragmenttabspoc/src/br/com/victoraldecoa/fragmenttabspoc/OsListFragment.java
@@ -8,39 +8,32 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class OsListFragment extends ListFragment{
-	
-    /** An array of items to display in ArrayList */
-    String os_versions[] = new String[]{
-            "Mountain Lion",
-            "Lion",
-            "Snow Leopard",
-            "Leopard",
-            "Tiger",
-            "Panther",
-            "Jaguar",
-            "Puma"
-    };
-    	
+public class OsListFragment extends ListFragment {
+
+	// An array of items to display in ArrayList
+	String os_versions[];
+
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
 		os_versions = getArguments().getStringArray("os_versions");
-		
-		/** Creating array adapter to set data in listview */
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getBaseContext(), android.R.layout.simple_list_item_multiple_choice, os_versions);
 
-        /** Setting the array adapter to the listview */
-        setListAdapter(adapter);
+		// Creating array adapter to set data in listview
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity()
+				.getBaseContext(),
+				android.R.layout.simple_list_item_multiple_choice, os_versions);
 
-		
+		// Setting the array adapter to the listview
+		setListAdapter(adapter);
+
 		return super.onCreateView(inflater, container, savedInstanceState);
 	}
-	
-	@Override
-    public void onStart() {
-            super.onStart();
 
-            /** Setting the multiselect choice mode for the listview */
-            getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-    }
+	@Override
+	public void onStart() {
+		super.onStart();
+
+		// Setting the multiselect choice mode for the listview
+		getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+	}
 }
